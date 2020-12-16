a554b5bb13fa1419a04971037982f87cad7a415d
manojts <manojmanuts81@gmail.com>
2020-12-16 15:32:45 +0530

    Stock span using stack

--- StockSpan.cpp
+++ StockSpan.cpp
@@ -1,58 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Stack{
-public:
-	int top;
-	int arr[50];
-	void push(int data){
-		top++;
-		arr[top] = data;
-	}
-	int pop(){
-		return arr[top--];
-	}
-};
-void PrintGreaterToLeft(Stack s,int arr[],int size,int count){
-	//cout<<1<<" ";
-	for(int i=0;i<size;i++){
-		count++;
-		int ans=0;
+	public:
+		int top;
+		int arr[50];
+
+		void push(int data){
+			arr[++top]=data;
+		}
+
+		int pop(){
+			return arr[top--];
+		}
+	};
+
+void StockSpan(Stack s,int arr[],int size){
+	Stack s1;
+	s1.top = -1;
+int res[size] = {-1};
+	for(int i = 0;i < size;i++)
+	{
 		if(s.top==-1){
 			s.push(arr[i]);
-			ans=1;
+			s1.push(i);
+			res[i] = -1;
 		}
-		else if(arr[i]<s.arr[s.top] ){
-			//cout<<s.arr[s.top]<<" ";
-			ans = i -s.top;
-			s.push(arr[i]);
-		}
-		else {
-			while(arr[i] > s.arr[s.top] && s.top>-1){
-				count++;
+		else
+		{
+			while(s.top>-1 && arr[i] > s.arr[s.top])
+			{
 				s.pop();
+				s1.pop();
 			}
-			if(s.top==-1){
-				//cout<<-1<<" ";
-				ans=i;
+			if(s.top == -1)
+			{
+				res[i] = -1;
 			}
-			else{
-				//cout<<s.arr[s.top]<<" ";
-				ans = i - s.top;
+			else
+			{
+				res[i] = s1.arr[s1.top];
 			}
 			s.push(arr[i]);
+			s1.push(i);
 		}
-		cout<<ans<<" ";
 	}
-	cout<<endl<<count<<endl;
+	for(int i = 0 ;i<size;i++){
+		cout<<i-res[i]<<" ";
+	}
 }
 int main(){
-
-	int arr[] = { 10, 4, 5, 6 ,90, 120, 80 ,100};
-	//1 1 2 3 5 6 1 2
-	int n = 8;
+	int arr[] = {100, 80, 60, 70, 60, 75, 85};
+	int size = 7;
 	Stack s;
-	s.top=-1;
-	PrintGreaterToLeft(s,arr,n,0);
-
-
+	s.top = -1;
+	Stack s1;
+	s1.top = -1;
+	StockSpan(s,arr,size);
+	cout<<endl;
 }
+// 1, 1, 1, 2, 1, 4, 6
